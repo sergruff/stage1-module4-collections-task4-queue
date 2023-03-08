9db1d2a46e4f4b7338c49181eec61a183dca682c
sergruff <sergruff@gmail.com>
2023-03-08 22:14:26 +0100

    s1m4collections-task4

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -5,19 +5,18 @@ import java.util.*;
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
         Queue<Integer> dishes = new PriorityQueue<>();
-        for (int i = 1; i < numberOfDishes; i++) {
+        for (int i = 1; i <= numberOfDishes; i++) {
             dishes.add(i);
         }
         List<Integer> resultList = new ArrayList<>();
-        int dishNumber;
-        int counter=0;
-        while (!dishes.isEmpty()){
+        int counter = 0;
+        while (!dishes.isEmpty()) {
             for (Integer dish : dishes) {
-                dishNumber = dish;
                 if (++counter % everyDishNumberToEat == 0) {
-                    resultList.add(dishNumber);
+                    resultList.add(dish);
                 }
             }
+            dishes.removeAll(resultList);
         }
         return resultList;
     }
